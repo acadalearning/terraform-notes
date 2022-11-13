4f1b3484e1b6bfab531a22d610afb8005a45c539
acadalearning <108498076+acadalearning@users.noreply.github.com>
2022-11-13 11:37:01 -0700

    Add files via upload

---
+++ 07 - Modules/example1-ec2/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = var.my_region
+  #profile = "default"
+}
+
+module "ec2module" {
+  source = "./ec2"
+  ec2name = "Demo-ec2"
+}
+
+output "module_output" {
+  value = module.ec2module.instance_id
+}
